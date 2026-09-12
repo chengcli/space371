caabaaa7d05ef5dbed28f26b9925d267c07cc72a
Cheng Li <chengcli@dart9>
2026-09-12 10:55:13 -0400

    wip

--- lib/refraction.py
+++ lib/refraction.py
@@ -9,3 +9,8 @@ def calc_refraction(ray_in: Ray, norm: np.ndarray, n_after: float) -> None | Ray
       return None
   return Ray(pos = ray_in.pos, dir = mu * ray_in.dir + (mu * c1 - np.sqrt(k)) * norm,
              n_in = n_after)
+
+def calc_reflection(ray_in: Ray, norm: np.ndarray, n_after: float) -> None | Ray:
+  c1 = -(norm @ ray_in.dir)
+  return Ray(pos = ray_in.pos, dir = ray_in.dir + 2. * mu * norm,
+             n_in = n_after)
